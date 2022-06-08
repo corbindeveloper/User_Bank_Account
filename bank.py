50c4e2fd005979278aa543874f61a9246b5b5233
Corbin Crawford <corbin.developer@gmail.com>
2022-06-08 13:41:34 -0600

    User Bank Account

--- bank.py
+++ bank.py
@@ -21,8 +21,35 @@ class BankAccount:
          self.balance = self.balance + (self.int_rate * self.balance)
       return self
 
-account = BankAccount(0.05, 0)
-account_two = BankAccount(0.1, 0)
+class User:	
+   bank_name = "First National Dojo"
 
-account.deposit(100).deposit(500).deposit(400).withdraw(900).yield_interest().display_account_info()
-account_two.deposit(100).deposit(500).withdraw(100).withdraw(100).withdraw(150).withdraw(150).yield_interest().display_account_info()
+   def __init__(self, name, email):
+      self.name = name
+      self.email = email
+      self.account = BankAccount(int_rate=0.1, balance=0)
+
+   # deposit
+   def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
+      self.account.balance += amount	# the specific user's account increases by the amount of the value received
+      return self
+
+   # withdrawal
+   def make_withdrawal(self, amount):
+      self.account.balance -= amount
+      return self
+
+   # display
+   def display_user_balance(self):
+      print(f"User: {self.name}, Balance {self.account.yield_interest().balance}")
+      return self.account.balance
+
+
+# ACCOUNTS
+guido = User("Guido van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+murphy = User("Murphy Merf", "murphy@merf.aol")
+
+monty.make_deposit(100).make_deposit(500).make_deposit(400).make_withdrawal(900).display_user_balance()
+guido.make_deposit(100).make_deposit(500).make_withdrawal(100).make_withdrawal(100).make_withdrawal(150).make_withdrawal(150).display_user_balance()
+murphy.make_deposit(100).make_deposit(500).make_deposit(1400).make_withdrawal(900).display_user_balance()
